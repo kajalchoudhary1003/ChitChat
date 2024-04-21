cd5aa35bb8b8ebd258b5e853d2964d494c3c25e6
Kajal Choudhary <kc8318258@gmail.com>
2024-04-22 00:23:39 +0530

    Update profile page with image upload and user update functionality

--- app/(root)/profile/page.jsx
+++ app/(root)/profile/page.jsx
@@ -1,5 +1,5 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@components/ui/input";
 import { Button } from "@components/ui/button";
@@ -7,30 +7,77 @@ import { Poppins } from "next/font/google";
 import { useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
 import { CldUploadButton } from "next-cloudinary";
+import Loader from "@components/Loader";
 
 const pop = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 const Profile = () => {
-    const {data: session} = useSession()
-    const user = session?.user;
+  const { data: session } = useSession();
+  const user = session?.user;
 
-    // react-hook-form
-    const {register, watch, setValue, handleSubmit, formState: {error}} = useForm(); 
+  const [loading, setLoading] = useState(true);
+   // react-hook-form
+   const {
+    register,
+    watch,
+    setValue,
+    reset,
+    handleSubmit,
+    formState: { error },
+  } = useForm();
 
-    const uploadPhoto = (result ) => {
-        setValue("profileImage", result?.info?.secure_url)
+  useEffect(() => {
+    if (user) {
+      reset({ username: user?.username, profileImage: user?.profileImage });
     }
-  return (
+    setLoading(false);
+  
+  }, [reset, user]);
+
+ 
+
+  const uploadPhoto = (result) => {
+    setValue("profileImage", result?.info?.secure_url);
+  };
+
+  const updateUser = async (data) => {
+    setLoading(true);
+    try{
+    const res = await fetch(`/api/users/${user._id}/update`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    setLoading(false);
+    window.location.reload();
+  }catch(err){
+    console.log(err);
+  };
+}
+
+  return loading ? (
+    <Loader />
+  ) : (
     // outer viewport
     <div className={`${pop.className} flex flex-row justify-center mt-20`}>
       {/* box */}
       <div className="flex flex-col items-center">
         <h1 className="font-bold text-3xl">Edit Your Profile</h1>
         {/* form */}
-        <form action="" className="space-y-4 mt-4">
+        <form onSubmit={handleSubmit(updateUser)} className="space-y-4 mt-4" >
           {/* input-username */}
           <div className="flex flex-row border border-slate-300 rounded-md px-3 py-1 my-2 w-full focus-within:border-slate-600 focus-within:border-2">
-            <Input {...register("username", {required: "Username is required", validate: (value) => {if(value.length < 3) {return "Username must be atleast 3 characters long"}}})}
+            <Input
+              {...register("username", {
+                required: "Username is required",
+                validate: (value) => {
+                  if (value.length < 3) {
+                    return "Username must be atleast 3 characters long";
+                  }
+                },
+              })}
               type="text"
               placeholder="Username"
               className="text-md  p-0 border-none focus-visible:ring-offset-0 focus-visible:ring-offset-transparent focus-visible:ring-transparent"
@@ -43,22 +90,40 @@ const Profile = () => {
               className="w-5 h-5 mt-2 stroke-slate-300"
             />
           </div>
+          {error?.username && (
+            <p className="text-red-500 text-sm">{error.username.message}</p>
+          )}
           {/* profile image */}
           <div className="flex flex-row justify-around">
             <Image
-              src={watch("profileImage") || user?.profileImage || "/assests/person.jpg"}
+              src={
+                watch("profileImage") ||
+                user?.profileImage ||
+                "/assests/person.jpg"
+              }
               alt="user-profile"
               width={180}
               height={180}
             />
             <div className="flex items-center ml-5">
-              <CldUploadButton options={{maxFiles: 1}} onUpload={uploadPhoto} uploadPreset="euk8fafl">
-              <p className="border-2 border-black bg-transparent text-black rounded-md p-3 hover:bg-slate-200 ">Upload New Image</p>
+              <CldUploadButton
+                options={{ maxFiles: 1 }}
+                onUpload={uploadPhoto}
+                uploadPreset="euk8fafl"
+              >
+                <p className="border-2 border-black bg-transparent text-black rounded-md p-3 hover:bg-slate-200 ">
+                  Upload New Image
+                </p>
               </CldUploadButton>
             </div>
           </div>
           {/* submit-button */}
-          <Button className="w-full bg-cyan-500 hover:bg-cyan-700 text-lg" type="submit" >Save Changes</Button>
+          <Button
+            className="w-full bg-cyan-500 hover:bg-cyan-700 text-lg"
+            type="submit"
+          >
+            Save Changes
+          </Button>
         </form>
       </div>
     </div>
@@ -66,3 +131,5 @@ const Profile = () => {
 };
 
 export default Profile;
+
+
---
+++ app/api/users/[userId]/update/route.js
@@ -0,0 +1,22 @@
+import { connectToDB } from "@mongodb";
+import User from "@models/User";
+
+export const POST = async (req, { params }) => {
+  try {
+    await connectToDB();
+    const { userId } = params;
+
+    const body = await req.json();
+    const { username, profileImage } = body;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { username, profileImage },
+      { new: true }
+    );
+    return new Response(JSON.stringify(updatedUser), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new Response("Failed to update User", { status: 500 });
+  }
+};
